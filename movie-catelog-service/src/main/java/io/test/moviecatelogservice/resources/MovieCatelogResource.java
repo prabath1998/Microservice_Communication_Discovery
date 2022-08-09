4d16d232c623015ce2954107b5743859816fad8d
prabath1998 <prabhathudayanga1998@gmail.com>
2022-08-09 11:02:44 +0530

    avoid returning lists

--- movie-catelog-service/src/main/java/io/test/moviecatelogservice/resources/MovieCatelogResource.java
+++ movie-catelog-service/src/main/java/io/test/moviecatelogservice/resources/MovieCatelogResource.java
@@ -3,6 +3,7 @@ package io.test.moviecatelogservice.resources;
 import io.test.moviecatelogservice.models.CatelogItem;
 import io.test.moviecatelogservice.models.Movie;
 import io.test.moviecatelogservice.models.Rating;
+import io.test.moviecatelogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -28,11 +30,8 @@ public class MovieCatelogResource {
     @RequestMapping("/{userId}")
     public List<CatelogItem> getCatelog(@PathVariable("userId") String userId){
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234",6),
-                new Rating("5678",8)
-        );
-         return ratings.stream().map(rating -> {
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId, UserRating.class);
+         return ratings.getUserRating().stream().map(rating -> {
              Movie movie =  restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
 
 //             Movie movie = webClientBuilder.build()
--- ratings-data-service/src/main/java/io/test/ratingsdataservice/resources/RatingsDataResource.java
+++ ratings-data-service/src/main/java/io/test/ratingsdataservice/resources/RatingsDataResource.java
@@ -1,6 +1,7 @@
 package io.test.ratingsdataservice.resources;
 
 import io.test.ratingsdataservice.models.Rating;
+import io.test.ratingsdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +19,13 @@ public class RatingsDataResource {
     }
 
     @RequestMapping("/users/{userId}")
-    public List<Rating> getUserRating(@PathVariable("userId") String userId){
+    public UserRating getUserRating(@PathVariable("userId") String userId){
         List<Rating> ratings = Arrays.asList(
                 new Rating("1234",6),
                 new Rating("5678",8)
         );
-        return ratings;
+         UserRating userRating =  new UserRating();
+         userRating.setUserRating(ratings);
+         return  userRating;
     }
 }
